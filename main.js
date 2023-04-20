23e410512a6b4f9347f25af1d7f497d27caa7776
francogaray <francogaray.dev@gmail.com>
2023-04-19 23:39:46 -0300

    Se agrega menu mobile responsive

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
-console.log(desktopMenu)
+const mobileMenuIco = document.querySelector('.menu-mobile-icon')
+const mobileMenu = document.querySelector('.mobile-menu')
+console.log(mobileMenu)
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+mobileMenuIco.addEventListener('click', tooggleMobileMenu )
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
 }
+function tooggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
+}
+
